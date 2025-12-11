f0456c6eb4acc64950a08f764307aee9a111ce31
jinw00-lee <jil527@ucsd.edu>
2025-12-10 21:55:43 -0800

    Complete!

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-# UCSD PSYC201A Reproduction Project: Kim & Kim (2022 *PNAS*)
+# Reproduction: Kim & Kim (2022 *PNAS*) | UCSD PSYC201A
 
 written by **Jinwoo Lee** ([jil527@ucsd.edu](jil527@ucsd.edu))
 
-This repository serves as a workspace for the Reproducibility Project in UCSD PSYC201A. The goal of this project is to reproduce the statistics of the following key result reported in **Kim & Kim (2022, *PNAS*)**:
+> [!NOTE]
+> - original paper: [https://doi.org/10.1073/pnas.2205162119](https://doi.org/10.1073/pnas.2205162119) 
+> - preregistration: [https://doi.org/10.17605/OSF.IO/38WV4](https://doi.org/10.17605/OSF.IO/38WV4)
+> - final reproduction report: [https://psyc-201.github.io/kim2022/](https://psyc-201.github.io/kim2022/)
 
-> Mantel tests revealed that the (young adults’) left amygdala–ventral PFC tract-morphology dissimilarity matrix was significantly correlated with the trait-anxiety dissimilarity matrix (P = 0.036).
+This repository serves as a workspace for the Reproduction Project in UCSD PSYC201A. The goal of this project is to reproduce the following key findings reported in **Kim & Kim (2022, *PNAS*)**:
+
+> **Younger adults:** “Mantel tests revealed that the left amygdala-ventral PFC tract-morphology dissimilarity matrix was significantly correlated with the trait-anxiety dissimilarity matrix.”          
+> **Older adults:** “Tests on the older adult sample also depicted a significant correlation between the trait-anxiety dissimilarity matrix and the left amygdala-ventral PFC tract-morphology dissimilarity matrix.”  
+
 
 ## Code Availability
 All dependencies required to run the code are stored in a **Pixi-based** virtual environment named ‘kim2024’ (note: “2024” is a typo of “2022” in the original naming). To set up and use the environment, please follow the steps below:     
 
 1. Clone the GitHub repository: `git clone https://github.com/psyc-201/kim2022.git`.    
 2. Activate the Pixi-based virtual environment in your local machine: `pixi shell`.    
-3. When using Jupyter, select the kernel named “**Python (kim2024)**”.       
+3. When using Jupyter, select the kernel named “**Python (kim2024)**” or "**R**".    
+
+Also, the key analytic package - `DIM` is on-going project in [UCSD SciMinds Research Studio](https://sciminds.stuido/). For this reason, the directory of `DIM` was not shared in this repository. For the sharing request, please contact the project lead: Jinwoo Lee ([jil527@ucsd.edu](jil527@ucsd.edu)).
 
 ## Data Availability    
-This project assumes the presence of preprocessed datasets directly shared by the original authors for reproduction purposes. To request access to the data, please contact Dr. Justin Minue Kim ([minuekim@skku.edu](minuekim@skku.edu)), the corresponding author of Kim & Kim (2022 *PNAS*). Once you receive the data, place it in a folder named `data-from-authors` located in the top-level directory of this project.     
+This project assumes the presence of two datasets:
+
+- **Preprocessed data for confirmatory analysis:** This data includes metadata, STAI-G-X2 self-report total scores, and tractgraphy-based imaging driven phenotypes. To request access to the data, please contact Dr. Justin Minue Kim ([minuekim@skku.edu](minuekim@skku.edu)), the corresponding author of Kim & Kim (2022 *PNAS*). Once you receive the data, place it in a folder named `data-from-authors` located in the top-level directory of this project.     
+
+- **Raw data for exploratory analysis:** Exploratory analysis uses additional self-report scores - Emotion Regulation Questionnaire (ERQ), NEO Five-Factor Inventory (NEO-FFI), and Impulsive Behavior Scale (UPPS). You can download the full dataset at [the official website of LEMON dataset](https://fcon_1000.projects.nitrc.org/indi/retro/MPI_LEMON.html). Once you download the data, place it in a folder named `Behavioural_Data_MPILMBB_LEMON` located in the top-level directory of this project. 
 
 ## Contact    
 For inquiries regarding this reproducibility project, please contact the project lead:
--- analysis/visualization.r
+++ analysis/visualization.r
@@ -1,221 +1,191 @@
-setwd("/Users/jinwoolee/Documents/github/kim2024/code/")
-
-library(parameters)
-library(tidyverse)
-library(psych)
-library(see)
-library(lavaan)
-library(performance)
-#library(semPlot)     # install error in pixi add r-
-#library(nFactors)    # install error in pixi add r-
-library(vegan)
-library(jsonlite)
-#library(Rtsne)       # install error in pixi add r-
 library(ggplot2)
-library(reshape)      # not included in the original code but needed for visualization
+library(patchwork)
 
-meta = read.csv("../data-from-authors/Meta.csv")
-STAI = read.csv("../data-from-authors/STAI_G_X2.csv")
+##### I. FIG 2: Confirmatory Analysis for Young Group ####
+conf_young_auth <- read.csv("results/confirmatory/youth_vectors_authors.csv") 
+conf_young_ours <- read.csv("results/confirmatory/youth_vectors_ours.csv")
 
-# filtering by age
-youth = subset(meta, Age == "20-25" | Age == "25-30" | Age == "30-35")
-
-# filtering by availability of MRI data
-youth = subset(youth, X != "sub-032339" & X != "sub-032341" & X != "sub-032459" & X != "sub-032370"
-               & X != "sub-032466" & X != "sub-032438" & X != "sub-032509")
-
-# filtering by SKID diagnoses
-youth$SKID_Diagnoses = as.factor(youth$SKID_Diagnoses)
-youth$SKID_Diagnoses = as.numeric(youth$SKID_Diagnoses)
-Hyouth = subset(youth, SKID_Diagnoses == 1 | SKID_Diagnoses == 10)
-
-# leaving only relevant info
-Hyouth = Hyouth[, -c(4:14, 16:21)]
-
-# merging STAI data
-Hyouth = merge(Hyouth, STAI, by = "X")
-
-# constructing the anxiety distance matrix
-HAnnaKSTAI = data.frame()
-for (i in 1:119) {
-  for (j in 1:119) {
-    HAnnaKSTAI[j, i] = (sum(Hyouth[i, 5], Hyouth[j, 5]))/2
-  }
-}
-
-
-yh_prob_5p_L = read.csv("../data-from-authors/tractfiles/yh_probmap_5p_L.csv", header = T)
-yh_prob_5p_L = yh_prob_5p_L[,-1]  # removing the first column which is just the index
-
-HAnnaKBrain = dist(t(yh_prob_5p_L))
-
-set.seed(42)
-results <- mantel(HAnnaKSTAI, HAnnaKBrain, method = "spearman", permutations = 10000)
-
-# Visualizing the scatterplot of pairwise STAI and Brain Dissimilarity
-HAnnaKSTAI_vec <- HAnnaKSTAI[upper.tri(HAnnaKSTAI)]
-HAnnaKBrain_vec <- HAnnaKBrain[upper.tri(HAnnaKBrain)]
-
-total_df <- data.frame(stai_mean = HAnnaKSTAI_vec, brain_dissimilarity = HAnnaKBrain_vec)
-
-orig_scatter <- ggplot(total_df, aes(x = stai_mean, y = brain_dissimilarity)) +
-  geom_point(alpha = 0.25, color = "gray", size = 0.75) +       
-  geom_smooth(method = "lm", se = TRUE, color = "#1565C0") + 
+fig2a <- ggplot(conf_young_auth, aes(x = anx_mean, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
   theme_classic() +
-  labs(
-    title = "",
-    x = "Pairwise STAI Mean",
-    y = "Pairwise Brain Dissimilarity"
-  ) +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
-  )
-
-orig_scatter
-
-perm.df <- data.frame(perm = results['perm'])
-
-ggplot(perm.df, aes(x = perm)) +
-  geom_histogram(bins = 100, color = "lightgray", fill = "lightgray") + 
-  geom_vline(xintercept = as.numeric(results['statistic']), color = "#1565C0", linewidth = 1) +
-  theme_classic()
+  labs(title = "Authors' script") + 
+  xlab("mean anxiety score") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(conf_young_auth$anx_mean), 
+           y = max(conf_young_auth$brain_dissimilarity),
+           label = "r = .148\n(P = .042)", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
   
-# =======
-
-dim_real_D_x <- read.csv("dim_real_D_x.csv", header = FALSE)
-dim_real_D_y <- read.csv("dim_real_D_y.csv", header = FALSE)
-
-dim_real_D_x_vec <- dim_real_D_x[upper.tri(dim_real_D_x)]
-dim_real_D_y_vec <- dim_real_D_y[upper.tri(dim_real_D_y)]
-
-dim_real_df <- data.frame(stai_mean = dim_real_D_x_vec, brain_dissimilarity = dim_real_D_y_vec)
-
-dim_real_scatter <- ggplot(dim_real_df, aes(x = stai_mean, y = brain_dissimilarity)) +
-  geom_point(alpha = 0.25, color = "gray", size = 0.75) +       
-  geom_smooth(method = "lm", se = TRUE, color = "#1565C0") + 
+fig2b <- ggplot(conf_young_ours, aes(x = anx_mean, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
   theme_classic() +
-  labs(
-    title = "",
-    x = "Pairwise STAI Mean",
-    y = "Pairwise Brain Dissimilarity"
-  ) +
+  labs(title = "Our DIM script") + 
+  xlab("mean anxiety score") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  annotate("text", x = min(conf_young_auth$anx_mean), 
+           y = max(conf_young_auth$brain_dissimilarity),
+           label = "r = .148\n(P = .043)", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
+
+fig2 <- fig2a + fig2b
+fig2
+
+ggsave("../writeup/figures/fig2.png", fig2, 
+       width = 6.8, height = 3.1,
+       units = "in", dpi = 300)
+
+
+##### II. FIG 3: Confirmatory Analysis for Older Group ####
+conf_older_auth <- read.csv("results/confirmatory/older_vectors_authors.csv") 
+conf_older_ours <- read.csv("results/confirmatory/older_vectors_ours.csv")
+
+fig3a <- ggplot(conf_older_auth, aes(x = anx_mean, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
+  theme_classic() +
+  labs(title = "Authors' script") + 
+  xlab("mean anxiety score") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(conf_older_auth$anx_mean), 
+           y = max(conf_older_auth$brain_dissimilarity),
+           label = "r = .294\n(P = .016)", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
+
+fig3b <- ggplot(conf_older_ours, aes(x = anx_mean, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
+  theme_classic() +
+  labs(title = "Our DIM script") + 
+  xlab("mean anxiety score") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  annotate("text", x = min(conf_older_auth$anx_mean), 
+           y = max(conf_older_auth$brain_dissimilarity),
+           label = "r = .294\n(P = .017)", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
+
+fig3 <- fig3a + fig3b
+fig3
+
+ggsave("../writeup/figures/fig3.png", fig3, 
+       width = 6.8, height = 3.1,
+       units = "in", dpi = 300)
+
+
+##### III. FIG 4: Exploratory Analysis within Discovery Set ####
+exp_young_ours <- read.csv("results/exploratory/discovery_vectors.csv") 
+exp_young_ours$comp_mean_minmax <- (exp_young_ours$comp_mean - min(exp_young_ours$comp_mean)) / (max(exp_young_ours$comp_mean) - min(exp_young_ours$comp_mean)) 
+conf_young_auth$anx_mean_minmax <- (conf_young_auth$anx_mean - min(conf_young_auth$anx_mean)) / (max(conf_young_auth$anx_mean) - min(conf_young_auth$anx_mean)) 
+
+summary(exp_young_ours)
+
+fig4a <- ggplot(conf_young_auth, aes(x = anx_mean_minmax, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
+  theme_classic() +
+  xlim(c(0, 1)) + 
+  labs(title = "Only Anxiety - Kim & Kim (2022)") + 
+  xlab("mean anxiety score (min/max scaled)") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(conf_young_auth$anx_mean_minmax), 
+           y = max(conf_young_auth$brain_dissimilarity),
+           label = "r = .148", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
+
+fig4b <- ggplot(exp_young_ours, aes(x = comp_mean_minmax, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
+  theme_classic() +
+  xlim(c(0, 1)) + 
+  labs(title = "Five Traits - Ours") + 
+  xlab("mean composite score (min/max scaled)") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(conf_young_auth$anx_mean_minmax), 
+           y = max(conf_young_auth$brain_dissimilarity),
+           label = "r = .278", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
+  
+fig4 <- fig4a + fig4b
+fig4
+
+ggsave("../writeup/figures/fig4.png", fig4, 
+       width = 6.8, height = 3.1,
+       units = "in", dpi = 300)
+
+
+##### IV. FIG 5: WEIGHTS INFERENCE ####
+exp_weights <- read.csv("results/exploratory/discovery_boot_weights.csv")
+exp_weights$feature <- c("anxiety", "reappraisal", "suppression", "neuroticism", "impulsivity")
+exp_weights_sorted <- exp_weights[order(-exp_weights$weights_estimates), ]
+exp_weights_sorted$feature <- factor(exp_weights_sorted$feature, levels = c("neuroticism", "suppression", "anxiety", "reappraisal", "impulsivity"))
+
+highlight_feature <- "suppression"
+
+fig5 <- ggplot(exp_weights_sorted, aes(x = feature, y = weights_estimates)) +
+  geom_errorbar(aes(ymin = weight_ci_lower, ymax = weight_ci_upper,
+                    color = feature == highlight_feature),
+                width = 0.0, size = 0.8, alpha = 0.8) +
+  geom_point(aes(color = feature == highlight_feature), size = 3) +
+  geom_text(aes(x = feature, y = weight_ci_upper + 0.02,
+    label = sprintf("%.3f", weights_estimates),
+    color = feature == highlight_feature
+  ), size = 3.5) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "gray50") +
+  scale_color_manual(values = c("TRUE" = "#1565C0",
+                                "FALSE" = "black")) + 
+  theme_classic() +
+  labs(y = "feature weights\n(bootstrapped 95% CI)", x = NULL) +
   theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
+    legend.position = "none",
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 11),
+    axis.title.y = element_text(size = 12)
   )
 
-dim_real_scatter
+ggsave("../writeup/figures/fig5.png", fig5, 
+       width = 3.0, height = 4.2,
+       units = "in", dpi = 300)
 
-dim_real_null <- read.csv("dim_real_null.csv", header = FALSE)
 
-ggplot(dim_real_null, aes(x = V1)) +
-  geom_histogram(bins = 100, color = "lightgray", fill = "lightgray") + 
-  geom_vline(xintercept = 0.148, color = "#1565C0", linewidth = 1) +
-  theme_classic()
+##### V. FIG 6: Exploratory Analysis to Replicate Set ####
+exp_older_ours <- read.csv("results/exploratory/replicate_vectors.csv") 
+exp_null <- read.csv("results/exploratory/replicate_null_distribution.csv")
+exp_older_ours$comp_mean_minmax <- (exp_older_ours$comp_mean - min(exp_older_ours$comp_mean)) / (max(exp_older_ours$comp_mean) - min(exp_older_ours$comp_mean)) 
 
-# ===
-
-dim_sim_D_x <- read.csv("dim_sim_D_x.csv")
-dim_sim_D_y <- read.csv("dim_sim_D_y.csv")
-
-dim_sim_D_x <- dim_sim_D_x[, -1]
-dim_sim_D_y <- dim_sim_D_y[, -1]
-
-dim_sim_D_x_vec <- dim_sim_D_x[upper.tri(dim_sim_D_x)]
-dim_sim_D_y_vec <- dim_sim_D_y[upper.tri(dim_sim_D_y)]
-
-dim_sim_df <- data.frame(stai_mean = dim_sim_D_x_vec, brain_dissimilarity = dim_sim_D_y_vec)
-
-dim_sim_scatter <- ggplot(dim_sim_df, aes(x = stai_mean, y = brain_dissimilarity)) +
-  geom_point(alpha = 0.25, color = "gray", size = 0.75) +       
-  geom_smooth(method = "lm", se = TRUE, color = "#1565C0") + 
+fig6a <- ggplot(exp_older_ours, aes(x = comp_mean_minmax, y = brain_dissimilarity)) +
+  geom_point(color = "lightgray", alpha = 0.30, size = 0.75) +
+  geom_smooth(method = "lm", se = FALSE, color = "#1565C0") + 
   theme_classic() +
-  labs(
-    title = "",
-    x = "Pairwise STAI Mean",
-    y = "Pairwise Brain Dissimilarity"
-  ) +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
-  )
+  xlim(c(0, 1)) + 
+  labs(title = "Replicate Set") + 
+  xlab("mean composite score (min/max scaled)") +
+  ylab("brain dissimilarity") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(conf_young_auth$anx_mean_minmax), 
+           y = max(conf_young_auth$brain_dissimilarity),
+           label = "r = .319\n(P = .011)", 
+           hjust = 0, vjust = 1, size = 4, color = "#1565C0")
 
-dim_sim_scatter
-
-dim_sim_null <- read.csv("dim_sim_null.csv", header = FALSE)
-dim_sim_null <- data.frame(dim_sim_null[-1, -1])
-
-ggplot(dim_sim_null, aes(x = dim_sim_null..1...1.)) +
-  geom_histogram(bins = 100, color = "lightgray", fill = "lightgray") + 
-  geom_vline(xintercept = 0.159, color = "#1565C0", linewidth = 1) +
-  theme_classic()
-
-# ===
-dim_train_D_x <- read.csv("learn_train_D_x.csv", header = FALSE)
-dim_train_D_y <- read.csv("learn_train_D_y.csv", header = FALSE)
-
-dim_train_D_x_vec <- dim_train_D_x[upper.tri(dim_train_D_x)]
-dim_train_D_y_vec <- dim_train_D_y[upper.tri(dim_train_D_y)]
-
-dim_train_df <- data.frame(psych_mean = dim_train_D_x_vec, brain_dissimilarity = dim_train_D_y_vec)
-
-dim_train_scatter <- ggplot(dim_train_df, aes(x = psych_mean, y = brain_dissimilarity)) +
-  geom_point(alpha = 0.25, color = "gray", size = 0.75) +       
-  geom_smooth(method = "lm", se = TRUE, color = "#1565C0") + 
-  theme_classic() +
-  labs(
-    title = "",
-    x = "Pairwise Composite Score Mean",
-    y = "Pairwise Brain Dissimilarity"
-  ) +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
-  )
-
-dim_train_scatter
-
-
-dim_test_D_x <- read.csv("learn_test_D_x.csv", header = FALSE)
-dim_test_D_y <- read.csv("learn_test_D_y.csv", header = FALSE)
-
-dim_test_D_x_vec <- dim_test_D_x[upper.tri(dim_test_D_x)]
-dim_test_D_y_vec <- dim_test_D_y[upper.tri(dim_test_D_y)]
-
-dim_test_df <- data.frame(psych_mean = dim_test_D_x_vec, brain_dissimilarity = dim_test_D_y_vec)
-
-dim_test_scatter <- ggplot(dim_test_df, aes(x = psych_mean, y = brain_dissimilarity)) +
-  geom_point(alpha = 0.25, color = "gray", size = 0.75) +       
-  geom_smooth(method = "lm", se = TRUE, color = "#1565C0") + 
-  theme_classic() +
-  labs(
-    title = "",
-    x = "Pairwise Composite Score Mean",
-    y = "Pairwise Brain Dissimilarity"
-  ) +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
-  )
-
-dim_test_scatter
-
-dim_test_null <- read.csv("perm.csv", header = FALSE)
-
-ggplot(dim_test_null, aes(x = V1)) +
+fig6b <- ggplot(exp_null, aes(x = null_rho)) +
   geom_histogram(bins = 100, color = "lightgray", fill = "lightgray") + 
   geom_vline(xintercept = 0.319, color = "#1565C0", linewidth = 1) +
   theme_classic()
 
-boot <- read.csv("learn_feature_weights.csv", header = FALSE)
-boot.upper <- read.csv("learn_feature_weights_upper.csv", header = FALSE)
-boot.lower <- read.csv("learn_feature_weights_lower.csv", header = FALSE)
+fig6 <- fig6a + 
+  inset_element(
+    p = fig6b, 
+    left = 0.6, 
+    bottom = 0.6,
+    right = 1,    
+    top = 1     
+  )
 
-boot.df <- cbind(boot, boot.lower)
-boot.df <- cbind(boot.df, boot.upper)
-colnames(boot.df) <- c("estimates", "CI_lower", "CI_upper")
-boot.df$name <- c("STAI", "ERQ_reappraisal", "ERQ_suppression", "FFI_neuroticism", "UPPS_preservence")
-
-ggplot(boot.df, aes(x = name, y = estimates)) +
-  geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper), width = 0.0) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "gray") +
-  ylim(c(-0.2, 0.3)) + 
-  theme_classic()
-  
+ggsave("../writeup/figures/fig6.png", fig6, 
+       width = 6.0, height = 4.2,
+       units = "in", dpi = 300)
